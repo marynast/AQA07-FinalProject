5a48bbb92b20d05837cd947f19e1551690e45561
Marina Stasevich <mstasev@gmail.com>
2021-01-04 19:01:10 +0300

    setting up DB

--- src/test/java/steps/ui/BaseStep.java
+++ src/test/java/steps/ui/BaseStep.java
@@ -1,5 +1,7 @@
 package steps.ui;
 
+import browserService.ReadProperties;
+import dataBaseService.DataBaseService;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import support.MyWebDriver;
@@ -8,9 +10,13 @@ import utils.Waiters;
 public class BaseStep {
     public final EventFiringWebDriver driver;
     public Waiters waiters;
+    public ReadProperties properties;
+    public DataBaseService dataBaseService;
 
     public BaseStep(MyWebDriver driver) {
         this.driver = driver;
         waiters = new Waiters(driver);
+        properties = new ReadProperties();
+        dataBaseService = new DataBaseService();
     }
 }
