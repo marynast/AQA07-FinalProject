650a8a4cebe7f67b00fd246d65005e827b0047e5
Marina Stasevich <mstasev@gmail.com>
2021-01-05 20:03:57 +0300

    updated ApiTest with getCurrentUserAFE feature steps

--- src/test/java/steps/api/ApiStep.java
+++ src/test/java/steps/api/ApiStep.java
@@ -8,6 +8,7 @@ import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import org.apache.http.HttpStatus;
 import org.apache.http.protocol.HTTP;
 import org.testng.Assert;
@@ -22,6 +23,7 @@ public class ApiStep extends BaseUtil {
     private String currentUser;
     private static Response response;
     private int status;
+    private String userName;
 
     @Step
     @Given("api is set up")
@@ -47,6 +49,17 @@ public class ApiStep extends BaseUtil {
                 .extract().response().getStatusCode();
     }
 
+    @When("admin sends GET request to get the current user")
+    public void adminSendsGETRequestTo() {
+        String endpoint = "/index.php?/api/v2/get_current_user";
+        this.userName =
+                given()
+                        .when()
+                        .get(endpoint)
+                        .then()
+                        .log().body()
+                        .extract().jsonPath().get("name");
+    }
 
     @Step
     @Then("status code is {int}")
@@ -57,7 +70,7 @@ public class ApiStep extends BaseUtil {
     @Step
     @Then("user name equals to {string}")
     public void getCurrentUser(String currentUser) {
-
+        Assert.assertEquals(userName, currentUser, "User name is incorrect");
     }
 }
 
