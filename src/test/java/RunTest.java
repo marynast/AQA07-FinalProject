de5c9c8dd9ee71cb707dbc3393b64823d991bb15
Marina Stasevich <mstasev@gmail.com>
2021-01-12 17:22:04 +0300

    working on parallel run in different browsers

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,9 +1,11 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 
 public class RunTest extends AbstractTestNGCucumberTests {
 
     @Override
+    @Parameters("browserType")
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
